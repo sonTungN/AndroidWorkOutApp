a75ea2dd0d6d3f319698088177496d35701038eb
sonTung Nguyen <nguyensontungtdn1901@gmail.com>
2024-11-06 10:54:05 +0700

    change: update text in edittext and adding activity intent

--- app/src/main/java/com/example/workoutapp/view/SignInActivity.java
+++ app/src/main/java/com/example/workoutapp/view/SignInActivity.java
@@ -1,5 +1,6 @@
 package com.example.workoutapp.view;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -7,16 +8,28 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
+import androidx.databinding.ViewDataBinding;
 
 import com.example.workoutapp.R;
+import com.example.workoutapp.databinding.ActivitySignInBinding;
 
 public class SignInActivity extends AppCompatActivity {
+
+    private ActivitySignInBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sign_in);
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_in);
+        binding.signupCta.setOnClickListener(view -> {
+            Intent i = new Intent(this, SignUpActivity.class);
+            startActivity(i);
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
--- app/src/main/java/com/example/workoutapp/view/SignUpActivity.java
+++ app/src/main/java/com/example/workoutapp/view/SignUpActivity.java
@@ -1,5 +1,6 @@
 package com.example.workoutapp.view;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.activity.EdgeToEdge;
@@ -7,16 +8,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.databinding.DataBindingUtil;
 
 import com.example.workoutapp.R;
+import com.example.workoutapp.databinding.ActivitySignUpBinding;
 
 public class SignUpActivity extends AppCompatActivity {
+
+    private ActivitySignUpBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_sign_up);
+
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_sign_up);
+        binding.signinCta.setOnClickListener(view -> {
+            Intent i = new Intent(this, SignInActivity.class);
+            startActivity(i);
+        });
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
